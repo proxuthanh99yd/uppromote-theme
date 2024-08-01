d795f0da6eea295b5cf02dc3a771fab053127d05
Mrlee <proxuthanh99yd@.com>
2024-08-01 12:03:00 +0700

    root

--- import-assets/import-css-js.php
+++ import-assets/import-css-js.php
@@ -18,7 +18,7 @@ function  wp_enqueue_local()
 	wp_enqueue_style('header', get_theme_file_uri('/template-parts/compound/header/assets/styles.css'));
 	wp_enqueue_style('footer', get_theme_file_uri('/template-parts/compound/footer/assets/styles.css'));
 
-	if (is_page('affiliate-marketing-3')) {
+	if (is_page('affiliate-marketing-2')) {
 		wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
 		wp_enqueue_script("swiper", 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', [], false, true);
 		wp_enqueue_script("collapse", get_theme_file_uri('/assets/bootstrap/js/src/collapse.js'), [], false, true);
--- template-render/page-affiliate-marketing.php
+++ template-render/page-affiliate-marketing.php
@@ -5,3 +5,4 @@ get_template_part('/template-parts/page-affiliate-marketing/section', 'benefit')
 get_template_part('/template-parts/page-affiliate-marketing/section', 'customer-words');
 echo do_shortcode('[sc_faqs]');
 echo do_shortcode('[sc_get_started]');
+?>
