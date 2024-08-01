9cd22069823e086f3e0722f8467e6b63151d26eb
Mrlee <proxuthanh99yd@.com>
2024-08-01 12:03:00 +0700

    root

--- functions.php
+++ functions.php
@@ -6,3 +6,17 @@ require get_theme_file_path('/import-assets/import-css-js.php');
 require get_theme_file_path('/inc/ajax.php');
 require get_theme_file_path('/inc/api.php');
 require get_theme_file_path('/inc/shortcodes.php');
+
+// add_action( 'phpmailer_init', function( $phpmailer ) {
+//     if ( !is_object( $phpmailer ) )
+//     $phpmailer = (object) $phpmailer;
+//     $phpmailer->Mailer     = 'smtp';
+//     $phpmailer->Host       = 'smtp.gmail.com';
+//     $phpmailer->SMTPAuth   = 1;
+//     $phpmailer->Port       = 587;
+//     $phpmailer->Username   = 'll0s3s0ll@gmail.com'; //điền tài khoản gmail của bạn
+//     $phpmailer->Password   = 'ieiz hqmu qnfv ebfw'; //điền mật khẩu ứng dụng mà bạn đã tạo ở trên
+//     $phpmailer->SMTPSecure = 'TLS';
+//     $phpmailer->From       = 'll0s3s0ll@gmail.com'; //điền tài khoản gmail của bạn
+//     $phpmailer->FromName   = 'Mr.Lee';
+// });
